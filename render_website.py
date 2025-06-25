d75770fa68634c6577b33db18dcce1500488ead7
Dmitriy877 <dmitriybukin05@gmail.com>
2025-06-25 21:56:12 +0300

    Split books list by pages.
    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,35 +1,38 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+import os
 
 from livereload import Server
 from more_itertools import chunked
 
+
 def rebuild():
 
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
+    os.makedirs('pages', exist_ok=True)
 
-    template = env.get_template('template.html')
-
-    with open('.\media\meta_data.json', encoding='utf-8') as file:
+    with open(os.path.join('media', 'meta_data.json'), encoding='utf-8') as file:
         books_json = file.read()
     books = json.loads(books_json)
-    chunked_books = list(chunked(books, 2))
 
-    # print(chunked_books)
-    
-    # for book in chunked_books:
-    #     for i in book:
-    #         print(i)
+    book_pages = list(chunked(books, 20))
 
-    rendered_page = template.render(
-        chunked_books=chunked_books
-    )
+    for i, book_page in enumerate(book_pages):
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        chunked_books = list(chunked(book_page, 2))
+
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+
+        template = env.get_template('./pages/template.html')
+
+        rendered_page = template.render(
+            chunked_books=chunked_books
+        )
+
+        with open(os.path.join('pages', f'index{i}.html'), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 rebuild()
@@ -38,4 +41,4 @@ server = Server()
 
 server.watch('template.html', rebuild)
 
-server.serve(root='.')
+server.serve(root='./pages')
